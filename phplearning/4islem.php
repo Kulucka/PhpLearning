9ade233ca1a1d88714a10f5b88a33756dc9a932b
yunusonal <yunus-deat@hotmail.com>
2016-03-23 14:31:33 +0200

    4islem

---
+++ phplearning/4islem.php
@@ -0,0 +1,28 @@
+<?php 
+	$sayi1=5;
+	$sayi2=9;
+	echo "Kullanilan rakamlar</br>";
+	echo "1.sayi :".$sayi1."</br>";
+	echo "2.sayi :".$sayi2."</br>";
+	echo "Sayilarin toplami :";
+	echo $sayi1+$sayi2."</br>";
+	echo "Sayilarin farki   :";
+	echo $sayi1-$sayi2."</br>";
+	echo "Sayilarin carpimi:";
+	echo $sayi1*$sayi2."</br>";
+	echo"Sayilarin bolumu:";
+	echo $sayi1/$sayi2."</br>";
+
+
+	
+
+
+
+
+
+
+
+
+
+
+ ?>
